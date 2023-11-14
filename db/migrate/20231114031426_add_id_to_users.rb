d036bc90ce0f6141c7eb305b474b9798b307051f
chororinko <147678517+chororinko@users.noreply.github.com>
2023-11-14 03:34:30 +0000

    [now] 4-11

---
+++ db/migrate/20231114031426_add_id_to_users.rb
@@ -0,0 +1,5 @@
+class AddIdToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :id, :integer
+  end
+end
---
+++ db/migrate/20231114031742_remove_id_to_users.rb
@@ -0,0 +1,4 @@
+class RemoveIdToUsers < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
